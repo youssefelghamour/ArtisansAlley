9a890059f76e869479ce45949d19c881ae4ce774
youssef <youssefelghamour@gmail.com>
2024-04-20 13:37:12 -0700

    add comment in setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,5 @@
 -- setup database and user for AirBnB project
+-- how to use it: cat setup_mysql_dev.sql | mysql -hlocalhost -uroot -p
 CREATE DATABASE IF NOT EXISTS artall_db;
 CREATE USER IF NOT EXISTS 'artall_user'@'localhost' IDENTIFIED BY 'artall_pwd';
 GRANT ALL PRIVILEGES ON artall_db.* TO 'artall_user'@'localhost';
