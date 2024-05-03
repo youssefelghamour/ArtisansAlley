3f0fd0021167bbaec96a5d7904b3312be7f2f27b
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-05-03 07:08:43 +0300

    product page redirect

--- web_flask/static/scripts/product.js
+++ web_flask/static/scripts/product.js
@@ -158,4 +158,11 @@ $(document).ready(function () {
         });
     }
 
+    $(document).on('click', '.products-item', function() {
+        // Get the product_id from the data-product-id when the product li is clicked
+        const product_id = $(this).data('product-id');
+        // Redirect the user to the product page with the specified product ID
+        window.location.href = `/product/${product_id}`;
+    });
+
 });
